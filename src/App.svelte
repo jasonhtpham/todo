9bc6c3e15a69287ba5c4861c5ac27c5d97486672
Jason Pham <htpham@deakin.edu.au>
2020-07-13 17:29:53 +1000

    No web3 error page

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,17 @@
 <script>
-  import Todos from './Todos.svelte'
+  import Todos from './Todos.svelte';
+  import Error from './Error.svelte';  
   import Web3 from 'web3';
   import todoABI from './todoABI.js';
 
   var web3 = window.web3;
+
+  //a variable to check if there is a web3 provider on user's browser
+  let noWeb3 = false;
+
+  let scAddress = "0x9f70f73ffe8c9a8d5635b01181efea83857daed9";
+  let todoSC;
+  let callerAddress;
 
 	export let todos = [
         {
@@ -35,35 +43,47 @@
 		console.log(event.detail.text);
 	}
 
-  //   function startApp() {
-  //       // etherGram = new web3.eth.Contract(ethergramABI,scAddress);
-  //   }
+  function startApp() {
+    todoSC = new web3.eth.Contract(todoABI,scAddress);
+  }
 
-  // window.addEventListener('load', async function() {
-  //   // Checking if Web3 has been injected by the browser (Mist/MetaMask)
-  //   if (typeof web3 !== 'undefined') {
-  //   // Use Mist/MetaMask's provider
-  //   console.log("Use MetaMask as provider");
-  //   web3 = new Web3(window.web3.currentProvider);
-  //   } else {
-  //     console.log("Error! There is no provider to use!!!");
-  //   }
+  window.addEventListener('load', async function() {
+    // Checking if Web3 has been injected by the browser (Mist/MetaMask)
+    if (typeof web3 !== 'undefined') {
+    // Use Mist/MetaMask's provider
+    // console.log(web3);
+    web3 = new Web3(window.web3.currentProvider);
 
-  //   // Now you can start your app & access web3js freely:
-  //   startApp()
+    //Get current user's address
+    var accounts = await web3.eth.getAccounts();
+    callerAddress = accounts[0];
 
-  //   var accounts = await web3.eth.getAccounts();
-  //   callerAddress = accounts[0];
+    var accountInterval = setInterval(async function() {
+      let accounts = await web3.eth.getAccounts();
+      if (accounts[0] !== callerAddress) {
+        callerAddress = accounts[0];
+      }
+    }, 100);
+    console.log(callerAddress);
 
-  //   var accountInterval = setInterval(async function() {
-  //     let accounts = await web3.eth.getAccounts();
-  //     if (accounts[0] !== callerAddress) {
-  //       callerAddress = accounts[0];
-  //     }
-  //   }, 100);
+    } else {
+      // console.log("Error! There is no provider to use!!!");
+      return noWeb3 = true;
+    }
 
-  // })
+    // Now you can start your app & access web3js freely:
+    try {
+    startApp()
+
+    } catch (e) {
+      console.log(e);
+    }
+  })
 
 </script>
 
-<Todos {todos} on:addTodo={addTodoSC} on:markCompleted={markCompletedSC} on:markCompletedAll={markCompletedAllSC}/>
+{#if noWeb3}
+  <Error />
+  {:else}
+    <Todos {todos} on:addTodo={addTodoSC} on:markCompleted={markCompletedSC} on:markCompletedAll={markCompletedAllSC}/>
+{/if}
---
+++ src/Error.svelte
@@ -0,0 +1,28 @@
+<script>
+    // import SlothImage from "./Sloth-branch.png";
+</script>
+
+<style>
+    .container {
+        text-align: center;
+        margin-top: 100px; 
+    }
+
+    .image {
+        /* height: 320px;
+        width: 271px; */
+        display: block;
+        margin: 100px auto;
+        width: 35%;
+    }
+</style>
+
+<div class="image">
+    <img src="images/Sloth-branch.png" alt="sloth pic" />
+</div>
+
+<div class="container">
+    <h3>No <i>"web3"</i> provider detected</h3>
+    <p>To use our web application, you will need a <i>"web3"</i> provider.
+    You can download and install <a href="https://metamask.io/">MetaMask</a> to be your <i>"web3"</i> provider.</p>
+</div>
